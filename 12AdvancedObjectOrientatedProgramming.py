6c39d79e23203bd934b15e3ae88681df36dab8a4
Idreesqbal <idreesqbal@gmail.com>
2020-10-18 23:38:19 +0200

    .

--- 12AdvancedObjectOrientatedProgramming.py
+++ 12AdvancedObjectOrientatedProgramming.py
@@ -45,9 +45,42 @@ class Emplyoee3():
         # if there exist another employees this change won't be applied to them
 
 # 3rd Topic of today: Class method and staticMethod
-    #Class methods are used to minupulate the class data it self.
+    # Class methods are used to minupulate the class data it self.
     # e.g.
     @classmethod
     def from_string(cls, emp_str):
         first, last, age = emp_str.split('-')
         return cls(first, last, age)
+    # ^ what is this code doing you might ask? imagine that you have a list of employees information
+    # and you wanted to take them all in, but they are all formatted as CSV file ...
+    # if u do them all manually then it costs a lots of time which we don't want.
+    # instead we can use the class method to do this job automatically so we don't have to do it.
+    # in that example above from_string method take emp_str as input, and then it splits them as
+    # first, last, pay just like a normal input of the class.
+
+    # Example1: new_emp_1 = Emplyoee3.from_string('john-doe-25')
+    @classmethod
+    def from_today(cls):
+        t = _time.time()
+        return cls.fromtimestamp(t)
+    @staticmethod # Static method is used as normal function that hasn't anything to do with the instance or the class
+
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+    #Example:
+    my_date = datetime.date(2016, 7, 11)
+    print(Employee3.is_workday(my_date))
+
+    def __add_(self, other): # this will able this class to be able to add two payment of employees
+        return self.pay + other.pay
+
+
+# Property decorator : it allows us to define a method but we can access it like attribute.
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+
+
